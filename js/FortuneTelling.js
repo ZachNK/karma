c064d1359d93e6e33aaac819669f167c9655f287
Namkyoo Kang <nkangzach@gmail.com>
2023-07-11 21:12:10 +0900

    Update FortuneTelling.js

--- js/FortuneTelling.js
+++ js/FortuneTelling.js
@@ -718,8 +718,6 @@ function this_year_coloring(startingYear, value_nowYear){
     }
 }
 
-
-
 function Copy(){
     var str = resultCopy + greatLuckCopy;
     CopyStringToClipboard(str);
@@ -736,44 +734,20 @@ function CopyStringToClipboard (string) {
     document.execCommand('copy');
 }
 
-// function Convert2Image(){
-//     let c = document.getElementById('result');
-//     let ctx = c.getContext('2d');
-//     ctx.reset();
-//     ctx.fillStyle = 'rgb(255, 255, 255)';
-//     ctx.fillRect(0, 0, 500, 750);
-
-//     ctx.font = '10pt 맑은고딕';
-//     ctx.fillStyle = 'rgb(0, 0, 0)';
-//     ctx.fillText(fullName, 75, 30);
-
-    
-//     ctx.fillText(resultCopy, 100, 100);
-//     base64String = c.toDataURL('image/jpeg');
-//     console.log(base64String);
-
-//     var link = document.createElement('a');
-//     link.href = base64String;
-//     link.download = today_date;
-//     link.click();
-//     //이름 초기화
-//     nameText = "";
-//     fullName = "";
-
-// }
 
 
+var canvas, context;
 function Convert2Image(){
-    let c = document.getElementById('canvas_result');
-    let ctx = c.getContext("2d");
+    canvas = document.getElementById("FRESH");
+    context = canvas.getContext("2d");
+
+    context.reset();
+    context.fillStyle = "rgba(255,255,255,1)";
+    context.fillRect(0, 0, 500, 500);
     
-    ctx.reset();
-    ctx.fillStyle = "rgba(255,255,255,1)";
-    ctx.fillRect(0, 0, 500, 750);
-    
-    ctx.font = "10pt 맑은고딕";
-    ctx.fillStyle = "rgba(0, 0, 0, 1)";
-    ctx.fillText(fullName, 50, 30);
+    context.font = "10pt 맑은고딕";
+    context.fillStyle = "rgba(0, 0, 0, 1)";
+    context.fillText(fullName, 50, 30);
 
     var m = [8];
     for (i=0; i<8; i++){
@@ -793,16 +767,14 @@ function Convert2Image(){
         
     }
 
-    m[0].onload = function() {ctx.drawImage(m[0], 50, 50, 80, 80);}
-    m[1].onload = function() {ctx.drawImage(m[1], 150, 50, 80, 80);}
-    m[2].onload = function() {ctx.drawImage(m[2], 250, 50, 80, 80);}
-    m[3].onload = function() {ctx.drawImage(m[3], 350, 50, 80, 80);}
-    m[4].onload = function() {ctx.drawImage(m[4], 50, 140, 80, 80);}
-    m[5].onload = function() {ctx.drawImage(m[5], 150, 140, 80, 80);}
-    m[6].onload = function() {ctx.drawImage(m[6], 250, 140, 80, 80);}
-    m[7].onload = function() {ctx.drawImage(m[7], 350, 140, 80, 80);}
-
-    
+    m[0].onload = function() {context.drawImage(m[0], 50, 50, 80, 80);}
+    m[1].onload = function() {context.drawImage(m[1], 150, 50, 80, 80);}
+    m[2].onload = function() {context.drawImage(m[2], 250, 50, 80, 80);}
+    m[3].onload = function() {context.drawImage(m[3], 350, 50, 80, 80);}
+    m[4].onload = function() {context.drawImage(m[4], 50, 140, 80, 80);}
+    m[5].onload = function() {context.drawImage(m[5], 150, 140, 80, 80);}
+    m[6].onload = function() {context.drawImage(m[6], 250, 140, 80, 80);}
+    m[7].onload = function() {context.drawImage(m[7], 350, 140, 80, 80);}
 
     var luck = [20];
     for(i=0; i<20; i++){
@@ -819,122 +791,33 @@ function Convert2Image(){
         
     }
 
-    luck[0].onload = function() {ctx.drawImage(luck[0], 410, 250, 40, 40);}
-    luck[1].onload = function() {ctx.drawImage(luck[1], 370, 250, 40, 40);}
-    luck[2].onload = function() {ctx.drawImage(luck[2], 330, 250, 40, 40);}
-    luck[3].onload = function() {ctx.drawImage(luck[3], 290, 250, 40, 40);}
-    luck[4].onload = function() {ctx.drawImage(luck[4], 250, 250, 40, 40);}
-    luck[5].onload = function() {ctx.drawImage(luck[5], 210, 250, 40, 40);}
-    luck[6].onload = function() {ctx.drawImage(luck[6], 170, 250, 40, 40);}
-    luck[7].onload = function() {ctx.drawImage(luck[7], 130, 250, 40, 40);}
-    luck[8].onload = function() {ctx.drawImage(luck[8], 90, 250, 40, 40);}
-    luck[9].onload = function() {ctx.drawImage(luck[9], 50, 250, 40, 40);}
-    luck[10].onload = function() {ctx.drawImage(luck[10], 410, 300, 40, 40);}
-    luck[11].onload = function() {ctx.drawImage(luck[11], 370, 300, 40, 40);}
-    luck[12].onload = function() {ctx.drawImage(luck[12], 330, 300, 40, 40);}
-    luck[13].onload = function() {ctx.drawImage(luck[13], 290, 300, 40, 40);}
-    luck[14].onload = function() {ctx.drawImage(luck[14], 250, 300, 40, 40);}
-    luck[15].onload = function() {ctx.drawImage(luck[15], 210, 300, 40, 40);}
-    luck[16].onload = function() {ctx.drawImage(luck[16], 170, 300, 40, 40);}
-    luck[17].onload = function() {ctx.drawImage(luck[17], 130, 300, 40, 40);}
-    luck[18].onload = function() {ctx.drawImage(luck[18], 90, 300, 40, 40);}
-    luck[19].onload = function() {ctx.drawImage(luck[19], 50, 300, 40, 40);}
+    luck[0].onload = function() {context.drawImage(luck[0], 410, 250, 40, 40);}
+    luck[1].onload = function() {context.drawImage(luck[1], 370, 250, 40, 40);}
+    luck[2].onload = function() {context.drawImage(luck[2], 330, 250, 40, 40);}
+    luck[3].onload = function() {context.drawImage(luck[3], 290, 250, 40, 40);}
+    luck[4].onload = function() {context.drawImage(luck[4], 250, 250, 40, 40);}
+    luck[5].onload = function() {context.drawImage(luck[5], 210, 250, 40, 40);}
+    luck[6].onload = function() {context.drawImage(luck[6], 170, 250, 40, 40);}
+    luck[7].onload = function() {context.drawImage(luck[7], 130, 250, 40, 40);}
+    luck[8].onload = function() {context.drawImage(luck[8], 90, 250, 40, 40);}
+    luck[9].onload = function() {context.drawImage(luck[9], 50, 250, 40, 40);}
+    luck[10].onload = function() {context.drawImage(luck[10], 410, 300, 40, 40);}
+    luck[11].onload = function() {context.drawImage(luck[11], 370, 300, 40, 40);}
+    luck[12].onload = function() {context.drawImage(luck[12], 330, 300, 40, 40);}
+    luck[13].onload = function() {context.drawImage(luck[13], 290, 300, 40, 40);}
+    luck[14].onload = function() {context.drawImage(luck[14], 250, 300, 40, 40);}
+    luck[15].onload = function() {context.drawImage(luck[15], 210, 300, 40, 40);}
+    luck[16].onload = function() {context.drawImage(luck[16], 170, 300, 40, 40);}
+    luck[17].onload = function() {context.drawImage(luck[17], 130, 300, 40, 40);}
+    luck[18].onload = function() {context.drawImage(luck[18], 90, 300, 40, 40);}
+    luck[19].onload = function() {context.drawImage(luck[19], 50, 300, 40, 40);}
 
-    // base64String = c.toDataURL('image/jpeg');
-    // console.log(base64String);
+    var dataURL = canvas.toDataURL("image/png");
 
-    // var link = document.createElement('a'); wkddo
-    // link.href = base64String;
-    // link.download = today_date;
-    // link.click();
-    // //이름 초기화
-    // nameText = "";
-    // fullName = "";
+    console.log(dataURL);
 }
 
 
 
-function downloading(){
-    var download = document.getElementById("download");
 
-    c = document.getElementById("canvas_result");
-    let ctx = c.getContext("2d");
-    
-    ctx.reset();
-    ctx.fillStyle = "rgba(255,255,255,1)";
-    ctx.fillRect(0, 0, 500, 750);
-    
-    ctx.font = "10pt 맑은고딕";
-    ctx.fillStyle = "rgba(0, 0, 0, 1)";
-    ctx.fillText(fullName, 75, 30);
 
-    var m = [8];
-    for (i=0; i<8; i++){
-        m[i] = new Image();
-        
-        var k = i%4;
-        var idN = '';
-        if(i<4){
-            idN = 'SKY';
-        }
-        else{
-            idN = 'LAND'
-        }
-
-        idN += k.toString();
-        m[i].src = document.getElementById(idN).src.split('karma/')[1];
-        
-    }
-
-    m[0].onload = function() {ctx.drawImage(m[0], 60, 50, 80, 80);}
-    m[1].onload = function() {ctx.drawImage(m[1], 160, 50, 80, 80);}
-    m[2].onload = function() {ctx.drawImage(m[2], 260, 50, 80, 80);}
-    m[3].onload = function() {ctx.drawImage(m[3], 360, 50, 80, 80);}
-    m[4].onload = function() {ctx.drawImage(m[4], 60, 140, 80, 80);}
-    m[5].onload = function() {ctx.drawImage(m[5], 160, 140, 80, 80);}
-    m[6].onload = function() {ctx.drawImage(m[6], 260, 140, 80, 80);}
-    m[7].onload = function() {ctx.drawImage(m[7], 360, 140, 80, 80);}
-
-    
-
-    var luck = [20];
-    for(i=0; i<20; i++){
-        luck[i] = new Image();
-        var k = '';
-        if(i<10){
-            k = 'i';
-        }
-        else{
-            k = 'p';
-        }
-        var n = (i%10)+1;
-        luck[i].src = document.getElementById(n.toString().padStart(3, '0') + k).src.split('karma/')[1];
-        
-    }
-
-    luck[0].onload = function() {ctx.drawImage(luck[0], 410, 250, 40, 40);}
-    luck[1].onload = function() {ctx.drawImage(luck[1], 370, 250, 40, 40);}
-    luck[2].onload = function() {ctx.drawImage(luck[2], 330, 250, 40, 40);}
-    luck[3].onload = function() {ctx.drawImage(luck[3], 290, 250, 40, 40);}
-    luck[4].onload = function() {ctx.drawImage(luck[4], 250, 250, 40, 40);}
-    luck[5].onload = function() {ctx.drawImage(luck[5], 210, 250, 40, 40);}
-    luck[6].onload = function() {ctx.drawImage(luck[6], 170, 250, 40, 40);}
-    luck[7].onload = function() {ctx.drawImage(luck[7], 130, 250, 40, 40);}
-    luck[8].onload = function() {ctx.drawImage(luck[8], 90, 250, 40, 40);}
-    luck[9].onload = function() {ctx.drawImage(luck[9], 50, 250, 40, 40);}
-    luck[10].onload = function() {ctx.drawImage(luck[10], 410, 300, 40, 40);}
-    luck[11].onload = function() {ctx.drawImage(luck[11], 370, 300, 40, 40);}
-    luck[12].onload = function() {ctx.drawImage(luck[12], 330, 300, 40, 40);}
-    luck[13].onload = function() {ctx.drawImage(luck[13], 290, 300, 40, 40);}
-    luck[14].onload = function() {ctx.drawImage(luck[14], 250, 300, 40, 40);}
-    luck[15].onload = function() {ctx.drawImage(luck[15], 210, 300, 40, 40);}
-    luck[16].onload = function() {ctx.drawImage(luck[16], 170, 300, 40, 40);}
-    luck[17].onload = function() {ctx.drawImage(luck[17], 130, 300, 40, 40);}
-    luck[18].onload = function() {ctx.drawImage(luck[18], 90, 300, 40, 40);}
-    luck[19].onload = function() {ctx.drawImage(luck[19], 50, 300, 40, 40);}
-
-    image = c.toDataURL("image/jpeg").replace("image/jpeg", "image/octet-stream");
-    //download.setAttribute("href", image);
-    //download.setAttribute("download", (today_date.toString()+".jpg"));
-    
-}
